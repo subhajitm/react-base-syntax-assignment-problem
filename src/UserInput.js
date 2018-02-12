09c65b6522fc549fc4d25d7bbe3b9ae15cd484fe
Subhajit Mandal <sumand@microsoft.com>
2018-02-12 00:02:33 -0800

    First Commit

---
+++ src/UserInput.js
@@ -0,0 +1,5 @@
+import React, { Component } from 'react';
+
+class UserInput extends Component {
+
+}
---
+++ src/UserOutput.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const userOutput = (props) => {
+    return (
+        <div>
+            <p>{props}</p>
+        </div>
+    )
+}
+
+export default userOutput;
